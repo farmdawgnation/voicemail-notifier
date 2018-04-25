f45335d50de1e577e155ff8db0d08fc530c7dbdd
Matt Farmer <matt@frmr.me>
2018-04-24 22:53:22 -0400

    Version bump clojure, add logging lib

--- project.clj
+++ project.clj
@@ -2,13 +2,14 @@
   :description "A simple service to notify me when Twilio gets a voicemail for me"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.4.0"]
                  [com.twilio.sdk/twilio "7.17.0"]
                  [com.typesafe/config "1.3.2"]
-                 [ch.qos.logback/logback-classic "1.2.3"]]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [org.clojure/tools.logging "0.4.0"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler voicemail-notifier.handler/app}
   :profiles
