277846a9b07a6893f7d20d4bb9a5131cd3ef35fb
Matt Farmer <matt@frmr.me>
2018-04-24 22:53:55 -0400

    Get request validation working

--- src/voicemail_notifier/middleware.clj
+++ src/voicemail_notifier/middleware.clj
@@ -1,16 +1,9 @@
 (ns voicemail-notifier.middleware
-  (:require [voicemail-notifier.config :as config])
+  (:require [voicemail-notifier.config :as config]
+            [clojure.tools.logging :as log]
+            [clojure.walk :as walk])
   (:import [com.twilio.security RequestValidator]
            [java.util HashMap]))
-
-(defn- add-param-to-map [hash-map param]
-  (let [key (-> param first (.toString))
-        value (second param)]
-    (.put hash-map param)
-    hash-map))
-
-(defn- to-params-map [params]
-  (reduce add-param-to-map (new HashMap) params))
 
 (defn wrap-request-validation
   "Middleware that will validate a request came from Twilio"
@@ -18,11 +11,17 @@
   (if config/validation-enabled?
     (fn [request]
       (let [validator (new RequestValidator config/auth-token)
-            url (:uri request)
-            verb (:request-method request)
-            params (:params request)
+            url (str config/base-url (:uri request))
+            params (walk/stringify-keys (:params request))
             twilio-sig (get (:headers request) "x-twilio-signature")]
+        (log/info "Verifying signature on request")
+        (log/info "URL: " url)
+        (log/info "Params: " params)
+        (log/info "Key set: " (.keySet params))
+        (log/info "Twilio's signature: " twilio-sig)
         (if (.validate validator url params twilio-sig)
-          (handler request)
-          {:status 401 :body {:error "Invalid signature."}})))
+          (do (log/info "Request was valid")
+              (handler request))
+          (do (log/info "Request was not valid")
+              {:status 401 :body {:error "Invalid signature."}}))))
     handler))
