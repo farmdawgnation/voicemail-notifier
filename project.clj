e2bfb03ab87f81c6de9a3806aa925e6a7023e0f6
Matt Farmer <matt@frmr.me>
2019-04-27 10:37:50 -0400

    Update url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject voicemail-notifier "0.1.0-SNAPSHOT"
   :description "A simple service to notify me when Twilio gets a voicemail for me"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/farmdawgnation/voicemail-notifier"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
