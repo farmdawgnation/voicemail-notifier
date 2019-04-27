d8a3f26dce410a214992cbf2d9d6582c05614dc0
Matt Farmer <matt@frmr.me>
2019-04-27 10:28:24 -0400

    Bump various deps

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "A simple service to notify me when Twilio gets a voicemail for me"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-defaults "0.3.2"]
                  [ring/ring-json "0.4.0"]
-                 [com.twilio.sdk/twilio "7.17.0"]
-                 [com.typesafe/config "1.3.2"]
+                 [com.twilio.sdk/twilio "7.37.3"]
+                 [com.typesafe/config "1.3.4"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [org.clojure/tools.logging "0.4.0"]]
-  :plugins [[lein-ring "0.12.4"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler voicemail-notifier.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
